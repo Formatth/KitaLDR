1b6c92f38e73b9f636f09cb88bc5437388331000
Rohmat Sukuriyanto <tatangsunandare@gmail.com>
2026-08-24 09:33:31 +0700

    fix: add AndroidX core for notifications

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ kotlin {
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.10.01"))
     implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3:1.3.1")
