0dea9a18ed6a05a2e400d06095942ea1aaf4ab55
Rohmat Sukuriyanto <tatangsunandare@gmail.com>
2026-08-25 10:28:46 +0700

    fix(build): use Coil compatible with compileSdk 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,8 +58,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     implementation("androidx.navigation:navigation-compose:2.8.4")
 
-    implementation("io.coil-kt.coil3:coil-compose:3.5.0")
-    implementation("io.coil-kt.coil3:coil-network-okhttp:3.5.0")
+    implementation("io.coil-kt:coil-compose:2.7.0")
 
     implementation(platform("com.google.firebase:firebase-bom:34.17.0"))
     implementation("com.google.firebase:firebase-auth")
